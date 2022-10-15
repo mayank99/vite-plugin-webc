f5588f077f6049d0c06cb7d50787e80a609a3519
Mayank <mayank99@users.noreply.github.com>
2022-10-14 22:04:54 -0400

    Add postcss nesting example

--- demo/vite.config.mjs
+++ demo/vite.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import VitePluginWebc from 'vite-plugin-webc';
+import postcssPresetEnv from 'postcss-preset-env';
 
 export default defineConfig({
 	plugins: [VitePluginWebc()],
+	css: {
+		postcss: {
+			plugins: [postcssPresetEnv({ features: { 'nesting-rules': true } })],
+		},
+	},
 });
